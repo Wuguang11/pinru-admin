16de191357a5f824dc3bc7eabf5676c840742440
Wuguang11 <1114860822@qq.com>
2022-07-21 00:15:46 +0800

    分类参数布局完成

--- src/views/addgoods/index.vue
+++ src/views/addgoods/index.vue
@@ -35,23 +35,96 @@
         tab-position="left"
       >
         <el-tab-pane label="基本信息" name="1">
-          <UserMangement></UserMangement>
+          <div style="display: auto">
+            <el-form
+              label-position="top"
+              :model="ruleForm"
+              status-icon
+              :rules="rules"
+              ref="ruleForm"
+              label-width="100px"
+              class="demo-ruleForm"
+            >
+              <el-form-item label="商品名称" prop="goods_name">
+                <el-input v-model="ruleForm.goods_name"></el-input>
+              </el-form-item>
+              <el-form-item label="商品价格" prop="goods_price">
+                <el-input v-model="ruleForm.goods_price"></el-input>
+              </el-form-item>
+              <el-form-item label="商品重量" prop="goods_weight">
+                <el-input v-model.number="ruleForm.goods_weight"></el-input>
+              </el-form-item>
+              <el-form-item label="商品数量" prop="goods_number">
+                <el-input v-model.number="ruleForm.goods_number"></el-input>
+              </el-form-item>
+              <el-form-item label="商品分类" prop="good_class">
+                <div class="block">
+                  <el-cascader
+                    :props="{ label: 'cat_name', value: 'cat_id' }"
+                    :options="options"
+                    filterable
+                    v-model="ruleForm.goods_class"
+                  ></el-cascader>
+                </div>
+              </el-form-item>
+            </el-form>
+          </div>
         </el-tab-pane>
-        <el-tab-pane label="商品参数" name="2">配置管理</el-tab-pane>
-        <el-tab-pane label="商品属性" name="3">角色管理</el-tab-pane>
-        <el-tab-pane label="商品图片" name="4">定时任务补偿</el-tab-pane>
-        <el-tab-pane label="商品内容" name="5">定时任务补偿</el-tab-pane>
+        <el-tab-pane label="商品参数" name="2">
+          <div v-for="item in goodsParamsData" :key="item.attr_id">
+            <p style="margin: 10px 0">{{ item.attr_name }}</p>
+            <el-checkbox
+              v-for="(item1, index) in item.attr_vals"
+              :key="index"
+              v-model="checked1"
+              :label="item1"
+              border
+            ></el-checkbox>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="商品属性" name="3">
+          <div v-for="item in goodsAttributeData" :key="item.attr_id">
+            <p style="margin: 15px 0">{{ item.attr_name }}</p>
+            <el-input v-model="item.attr_vals"></el-input>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="商品图片" name="4">
+          <el-upload
+            class="upload-demo"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :on-preview="handlePreview"
+            :on-remove="handleRemove"
+            :file-list="fileList"
+            list-type="picture"
+          >
+            <el-button size="small" type="primary">点击上传</el-button>
+          </el-upload>
+        </el-tab-pane>
+        <el-tab-pane label="商品内容" name="5"
+          ><editor-bar
+            :catchData="catchData"
+            :content="editorContent"
+          ></editor-bar
+        ></el-tab-pane>
       </el-tabs>
     </el-card>
   </div>
 </template>
 
 <script>
+import EditorBar from '@/components/EditorItem.vue'
 import BreadCrumb from '@/components/BreadCrumb.vue'
-import UserMangement from '@/views/addgoods/components/UserManagement.vue'
+import { getcategories, getGoodsAttribute } from '@/api/addgoods'
 export default {
   name: 'AddGoods',
-  created () { },
+  created () {
+    // 获取基本信息的数据
+    this.getcategories()
+    // 获取产品参数的信息
+    this.getGoodsParams()
+    // 获取商品信息
+    this.getGoodsAttribute()
+  },
   data () {
     return {
       menus: {
@@ -64,7 +137,44 @@ export default {
       // 控制步骤条的步骤
       active: 0,
       // 控制侧边栏的标签页
-      activeName: '1'
+      activeName: '1',
+      // 基本信息部分数据
+      // 基本信息输入的内容
+      ruleForm: {
+        goods_name: '',
+        goods_price: 0,
+        goods_weight: 0,
+        goods_number: 0,
+        goods_class: ''
+      },
+      // 基本信息表单验证规则
+      rules: {
+        goods_name: [
+          { required: true, message: '商品名称不能为空', trigger: 'blur' }
+        ],
+        goods_price: [
+          { required: true, message: '商品价格不能为空', trigger: 'blur' }
+        ],
+        goods_weight: [
+          { required: true, message: '商品重量不能为空', trigger: 'blur' }
+        ],
+        goods_number: [
+          { required: true, message: '商品数量不能为空', trigger: 'blur' }
+        ],
+        goods_class: [
+          { required: true, message: '商品数量不能为空', trigger: 'blur' }
+        ]
+      },
+      //  级联选择器数据
+      options: [],
+      // 商品参数部分数据
+      checked1: true,
+      goodsParamsData: [],
+      // 商品属性部分数据
+      goodsAttributeData: [],
+      // 图片上传部分数据
+      fileList: []
+
     }
   },
   methods: {
@@ -73,12 +183,47 @@ export default {
       console.log(tab, event)
       console.log(tab._uid)
       this.active = this.activeName - 1
+    },
+
+    // 请求获得基本信息分类数据
+    async getcategories () {
+      const { data: res } = await getcategories()
+      this.options = res.data
+    },
+    // 商品参数部分请求数据
+    async getGoodsParams () {
+      const { data: res } = await getGoodsAttribute(6, { sel: 'many' })
+      res.data.forEach(item => {
+        item.attr_vals = item.attr_vals.split(' ')
+      })
+      this.goodsParamsData = res.data
+      // console.log(res.data)
+    },
+    async getGoodsAttribute () {
+      const { data: res } = await getGoodsAttribute(6, { sel: 'only' })
+      console.log(res.data)
+      this.goodsAttributeData = res.data
+    },
+    // 图片上传的回调
+    handleRemove (file, fileList) {
+      console.log(file, fileList)
+    },
+    handlePreview (file) {
+      console.log(file)
+    },
+    // 监听富文本的输入
+    catchData (e) {
+      console.log('e=====?>', e)
+    },
+    // 富文本中的内容
+    editorContent (e) {
+      console.log('e=====?>', e)
     }
   },
   computed: {},
   watch: {},
   filters: {},
-  components: { BreadCrumb, UserMangement }
+  components: { BreadCrumb, EditorBar }
 }
 </script>
 
--- src/views/sortparams/index.vue
+++ src/views/sortparams/index.vue
@@ -1,6 +1,69 @@
 <template>
   <div>
     <BreadCrumb :menus="$store.state.menusInfo[2]" index="1"></BreadCrumb>
+    <el-card style="margin-top: 15px">
+      <el-alert
+        title="注意：只允许为第三级分类设置相关参数"
+        type="warning"
+        show-icon
+        :closable="false"
+      >
+      </el-alert>
+      <!-- 级联选择器 -->
+      <div class="block">
+        <span class="demonstration">请选择商品分类：</span>
+        <el-cascader
+          placeholder="试试搜索：指南"
+          :options="options"
+          filterable
+        ></el-cascader>
+      </div>
+      <!-- 底部标签页 -->
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="用户管理" name="first">
+          <el-button type="primary" size="mini" style="margin-bottom: 15px"
+            >添加属性</el-button
+          >
+          <el-table
+            :data="tableData"
+            style="width: 100%; font-size: 14px"
+            border
+          >
+            <el-table-column type="expand">
+              <template slot-scope="props">
+                <el-form label-position="left" inline class="demo-table-expand">
+                  <el-form-item label="商品名称">
+                    <span>{{ props.row.name }}</span>
+                  </el-form-item>
+                  <el-form-item label="所属店铺">
+                    <span>{{ props.row.shop }}</span>
+                  </el-form-item>
+                  <el-form-item label="商品 ID">
+                    <span>{{ props.row.id }}</span>
+                  </el-form-item>
+                  <el-form-item label="店铺 ID">
+                    <span>{{ props.row.shopId }}</span>
+                  </el-form-item>
+                </el-form>
+              </template>
+            </el-table-column>
+            <el-table-column label="#" prop="id" width="58"> </el-table-column>
+            <el-table-column label="分类名称" prop="name"> </el-table-column>
+            <el-table-column label="操作" prop="desc">
+              <template>
+                <el-button size="small" icon="el-icon-edit" type="primary"
+                  >编辑</el-button
+                >
+                <el-button size="small" type="danger" icon="el-icon-delete"
+                  >删除</el-button
+                >
+              </template></el-table-column
+            >
+          </el-table>
+        </el-tab-pane>
+        <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+      </el-tabs>
+    </el-card>
   </div>
 </template>
 
@@ -10,9 +73,84 @@ export default {
   name: 'sortparams',
   created () { },
   data () {
-    return {}
+    return {
+      // 级联选择器数据
+      options: [{
+        value: 'zhinan',
+        label: '指南',
+        children: [{
+          value: 'shejiyuanze',
+          label: '设计原则',
+          children: [{
+            value: 'yizhi',
+            label: '一致'
+          }, {
+            value: 'fankui',
+            label: '反馈'
+          }, {
+            value: 'xiaolv',
+            label: '效率'
+          }, {
+            value: 'kekong',
+            label: '可控'
+          }]
+        }, {
+          value: 'daohang',
+          label: '导航',
+          children: [{
+            value: 'cexiangdaohang',
+            label: '侧向导航'
+          }, {
+            value: 'dingbudaohang',
+            label: '顶部导航'
+          }]
+        }]
+      }],
+      // 底部标签页的数据
+      activeName: 'first',
+      // 表格数据
+      tableData: [{
+        id: '12987122',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333'
+      }, {
+        id: '12987123',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333'
+      }, {
+        id: '12987125',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333'
+      }, {
+        id: '12987126',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333'
+      }]
+    }
   },
-  methods: {},
+  methods: {
+    // 底部标签页点击触发
+    handleClick (tab, event) {
+      console.log(tab, event)
+    }
+
+  },
   computed: {},
   watch: {},
   filters: {},
@@ -21,4 +159,22 @@ export default {
 </script>
 
 <style scoped lang='less'>
+.block {
+  margin: 15px 0;
+  .demonstration {
+    font-size: 14px;
+  }
+}
+.demo-table-expand {
+  font-size: 0;
+}
+.demo-table-expand label {
+  width: 90px;
+  color: #99a9bf;
+}
+.demo-table-expand .el-form-item {
+  margin-right: 0;
+  margin-bottom: 0;
+  width: 50%;
+}
 </style>
