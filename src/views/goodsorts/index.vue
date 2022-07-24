944fd9fec07fbe062c4608b01b24cff16e5b47eb
Wuguang11 <1114860822@qq.com>
2022-07-24 13:42:49 +0800

    解决了一些bug

--- src/views/goodsorts/index.vue
+++ src/views/goodsorts/index.vue
@@ -1,48 +1,6 @@
 <template>
   <div>
     <BreadCrumb :menus="$store.state.menusInfo[2]" index="2"></BreadCrumb>
-    <el-card style="margin-top: 15px">
-      <!-- 按钮 -->
-      <el-button type="primary">添加分类</el-button>
-      <!-- 表格 -->
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column type="expand" prop="id" width="80">
-          <template slot-scope="props">
-            <el-form
-              label-position="left"
-              inline
-              class="demo-table-expand"
-              label="商品 ID"
-            >
-              <el-form-item label="商品名称">
-                <span>{{ props.row.name }}</span>
-              </el-form-item>
-              <el-form-item label="所属店铺">
-                <span>{{ props.row.shop }}</span>
-              </el-form-item>
-              <el-form-item label="商品 ID">
-                <span>{{ props.row.id }}</span>
-              </el-form-item>
-              <el-form-item label="店铺 ID">
-                <span>{{ props.row.shopId }}</span>
-              </el-form-item>
-              <el-form-item label="商品分类">
-                <span>{{ props.row.category }}</span>
-              </el-form-item>
-              <el-form-item label="店铺地址">
-                <span>{{ props.row.address }}</span>
-              </el-form-item>
-              <el-form-item label="商品描述">
-                <span>{{ props.row.desc }}</span>
-              </el-form-item>
-            </el-form>
-          </template>
-        </el-table-column>
-        <el-table-column label="商品 ID" prop="id"> </el-table-column>
-        <el-table-column label="商品名称" prop="name"> </el-table-column>
-        <el-table-column label="描述" prop="desc"> </el-table-column>
-      </el-table>
-    </el-card>
     <el-button
       type="primary"
       size="small"
@@ -147,7 +105,6 @@
         <el-button type="primary" @click="hanleDefine">确 定</el-button>
       </span>
     </el-dialog>
-    >>>>>>> sortparams
   </div>
 </template>
 
